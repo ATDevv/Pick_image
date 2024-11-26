3efe1984c5b2d984631215249723bc7aa1c0bc1b
Tuấngg Anhhh <macofanh@Tuongg-Anhh.local>
2024-11-27 04:24:58 +0700

    use State && multi style

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,36 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router'; 
+import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
+import { Link } from 'expo-router';
+import { useState } from 'react';
 
 export default function Index() {
+
+  const [name, setName] = useState<string>('')
+
+  const handleOnClick = (e: any): void => {
+    setName(e.target.value)
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
+      <View>
+        <TextInput
+          value={name}
+          placeholder='Enter name'
+          onChange={handleOnClick}
+          style={styles.text}
+        />
+      </View>
+      <Text style={styles.text}>Home screen of {name}</Text>
+      <Link href="/about" style={{ ...styles.button, textDecorationLine: 'none', borderWidth: 3, borderColor: 'yellow' }}>
         Go to About screen
       </Link>
+      <View>
+        <Button
+          title='Increase'
+          onPress={() => {
+            alert('me')
+          }} />
+      </View>
     </View>
   );
 }
@@ -18,13 +41,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+    // marginBottom: 55,
   },
   text: {
     color: '#fff',
+    marginBottom: 15
   },
   button: {
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
+    marginBottom: 15
+
   },
 });
